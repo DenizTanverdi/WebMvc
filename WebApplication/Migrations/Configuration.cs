b53fd55d0d32e0fd6966a987eeaeecbb0c796835
semvsde <semvs@ayvansaray.edu.tr>
2019-02-07 13:56:12 +0300

    DbsET

--- WebApplication/Migrations/Configuration.cs
+++ WebApplication/Migrations/Configuration.cs
@@ -9,7 +9,8 @@ namespace WebApplication.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(WebApplication.Models.AppDbContext context)
--- WebApplication/Models/AppDbContext.cs
+++ WebApplication/Models/AppDbContext.cs
@@ -8,9 +8,12 @@ namespace WebApplication.Models
 {
     public class AppDbContext:DbContext
     {
-        public AppDbContext() : base("StuCon")
+        public AppDbContext() : base("StuConn")
         {
            Database.SetInitializer(new MigrateDatabaseToLatestVersion<AppDbContext, Migrations.Configuration>("StuConn"));
         }
+        public DbSet<Student> Student { get; set; }
+        public DbSet<Teacher> Teacher { get; set; }
+        public DbSet<ClassRoom> Classroom { get; set; }
     }
 }
